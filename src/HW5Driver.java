c18bd4d2be21bbe63765d91a9c6d48995337cd23
Redl3lood <raymond4808@gmail.com>
2023-03-01 15:19:05 -0600

    Pre-Final, code optimization |  needs comments for methods purpose

--- src/HW5Driver.java
+++ src/HW5Driver.java
@@ -1,21 +1,37 @@
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class HW5Driver {
     // return the index of the MyPoint element in the list that's
     // closest to the origin (0, 0)
     public static int findClosestToOrigin(ArrayList<MyPoint> list){
         MyPoint zero = new MyPoint(0, 0);
-        int holder=0;
-        double highNumber=9999999.0;
+        int indexHolder=0;
+        double highNumber=list.get(0).distanceBetween(zero);
 
-        for (int i = 0; i< list.size(); i++) {
+        for (int i = 1; i< list.size(); i++) {
             double checker= list.get(i).distanceBetween(zero);
             if (checker < highNumber) {
                 highNumber= checker;
-                holder = i ;
+                indexHolder = i ;
             }
         }
-        return holder;
+        return indexHolder;
+    }
+
+    // Creates and returns a list of MyPoint objects
+    private static ArrayList<MyPoint> createPointList() {
+        ArrayList<MyPoint> list = new ArrayList<>();
+        int count = 10;
+        int min = -20, max = 20;
+
+// create points out of (-20~20, -20~20) range
+        for (int i=0; i<count; i++) {
+            int x = ThreadLocalRandom.current().nextInt(min, max+1);
+            int y = ThreadLocalRandom.current().nextInt(min, max+1);
+            list.add(new MyPoint(x, y));
+        }
+        return list;
     }
 
     public static void main(String[] args) {
@@ -36,21 +52,23 @@ public class HW5Driver {
         System.out.println("Distance between p1 and p2: " + p1.distanceBetween(p2));
         // result should be: 8.246211…
 
-        MyPoint arrayLister = new MyPoint (0,0);
-        ArrayList <MyPoint> arrayListerArraylist = arrayLister.createPointList();
+        ArrayList <MyPoint> arrayListerArraylist= createPointList();;
         //Code below displays all Point objects in the list in coordinate value
-        /*for (MyPoint location:P1Arraylist){
+        /*for (MyPoint location:arrayListerArraylist){
             System.out.println(location);
         }*/
+        System.out.println();
         System.out.println("The point closest to (0, 0) is: (" +arrayListerArraylist.get(findClosestToOrigin(arrayListerArraylist))+")");
         System.out.println();
 
+        //create two Shape Objects with MyPoint objects values plus color
         Shape s1 = new Shape(p1, "Blue");
         Shape s2= new Shape(p2, "Red");
-
         System.out.println("S1 "+s1);
         System.out.println("S2 "+s2);
+
         System.out.println("Distance between s1 and s2 = "+s1.distanceBetweenCenters(s2));
-    } // end main distanceBetweenCenters
-        //Question 3 of the page
+        // result should be: 8.246211…
+    } // end main
+
 }
